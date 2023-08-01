bae2c308e21e9e7b8dc53994ed35187cb40e3887
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-01 08:19:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
+st.set_page_config(
+  page_title="Equipment Dashboard",
+  page_icon=":bar_chart:",
+  layout="wide"                 
+)
+
+# MAINPAGE
+
+st.header(":space_invader: MVC Equipment Schedule Automation")
+st.markdown("##")
+
+
 hide_st_style="""
             <style>
             #MainMenu {visibility:hidden;}
