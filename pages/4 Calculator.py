b90ad3a50f3a447669410db7c2848406728ed4a7
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-21 08:58:39 -0600

    Update 4 Calculator.py

--- pages/4 Calculator.py
+++ pages/4 Calculator.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.title("Calculator")
+st.title("VA Calculator")
 st.write("---")
 
 num1 = st.number_input(label = 'Load')
 
 operation = st.radio('Select an Operation to Perform:',
-                     ('120/1','120/3','208/1','208/3','277/1','277,3','480/1','480/3'))
+                     ('120/1','120/3','208/1','208/3','277/1','277/3','480/1','480/3'))
 
-def calculator():
+def VAcalculator():
   if operation == '120/1':
     ans = round((num1 * 120), 2)
   elif operation == '120/3':
@@ -32,6 +32,6 @@ def calculator():
  
 
 if st.button('Calculate'):
-  calculator()
+  VAcalculator()
 
 
