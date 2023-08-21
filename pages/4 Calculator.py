1604a04ea47e99ce228eb93a57c167a2045baa45
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-21 08:50:09 -0600

    Update 4 Calculator.py

--- pages/4 Calculator.py
+++ pages/4 Calculator.py
@@ -28,4 +28,6 @@ def calculator():
   else:
     st.warning("Division by 0 error. Please enter a non-zero number.")
     ans = "Not defined"
-   st.success('Answer = ' + str(ans))
+return ans
+st.success('Answer = ' + ans)
+
