e15c466ee46fefb3a4aa2b862822513812ded1f4
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-21 10:23:25 -0600

    Update 4 Calculator.py

--- pages/4 Calculator.py
+++ pages/4 Calculator.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+  page_title="Calculators",
+  page_icon=":bar_chart:",
+  layout="wide"                 
+)
+
 st.title("VA Calculator")
 st.write("---")
 CAtype = st.selectbox('Select Desired Calculator',
