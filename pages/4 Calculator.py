d9c75f18c4193d3dd1276466c3655160b3866e0e
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-21 09:08:30 -0600

    Update 4 Calculator.py

--- pages/4 Calculator.py
+++ pages/4 Calculator.py
@@ -2,36 +2,36 @@ import streamlit as st
 
 st.title("VA Calculator")
 st.write("---")
+CAtype = st.dropdown(label = 'Select Desirec Calculator',
+            'VA Calculator', 'HP Calculator')
+if CAtype == 'VA Calculator'
+  num1 = st.number_input(label = 'Load')
 
-num1 = st.number_input(label = 'Load')
-
-operation = st.radio('Select an Operation to Perform:',
+  operation = st.radio('Select an Operation to Perform:',
                      ('120/1','120/3','208/1','208/3','277/1','277/3','480/1','480/3'))
 
-def VAcalculator():
-  if operation == '120/1':
-    ans = round((num1 * 120), 2)
-  elif operation == '120/3':
-    ans = round((num1 * 208), 2)
-  elif operation == '208/1':
-    ans = round((num1 * 208), 2) 
-  elif operation == '208/3':
-    ans = round((num1 * 360), 2)  
-  elif operation == '277/1':
-    ans = round((num1 * 277), 2) 
-  elif operation == '277/3':
-    ans = round((num1 * 480), 2)                     
-  elif operation == '480/1':
-    ans = round((num1 * 480), 2)                   
-  elif operation == '480/3':
-    ans = round((num1 * 831), 2) 
-  else:
-    st.warning("Division by 0 error. Please enter a non-zero number.")
-    ans = "Not defined"
-  st.success('Answer = ' + str(ans))
+  def VAcalculator():
+    if operation == '120/1':
+      ans = round((num1 * 120), 2)
+    elif operation == '120/3':
+      ans = round((num1 * 208), 2)
+    elif operation == '208/1':
+      ans = round((num1 * 208), 2) 
+    elif operation == '208/3':
+      ans = round((num1 * 360), 2)  
+    elif operation == '277/1':
+      ans = round((num1 * 277), 2) 
+    elif operation == '277/3':
+      ans = round((num1 * 480), 2)                     
+    elif operation == '480/1':
+      ans = round((num1 * 480), 2)                   
+    elif operation == '480/3':
+      ans = round((num1 * 831), 2) 
+    else:
+      st.warning("Division by 0 error. Please enter a non-zero number.")
+      ans = "Not defined"
+    st.success('Answer = ' + str(ans))
  
-
-if st.button('Calculate'):
-  VAcalculator()
-
+  if st.button('Calculate'):
+    VAcalculator()
 
