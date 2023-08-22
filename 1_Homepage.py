5aff272a3bb07d1adcb4aa9f5ae430ced1990b2d
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-22 13:03:30 -0600

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import base64
+from pathlib import path
 # st.set_page_config(
 #   page_title="MVC Tools - Homepage",
 #   page_icon=":bar_chart:",
@@ -19,18 +20,29 @@ import base64
 #             header {visibility: hidden;}
 #             </style>
 #             """
-
-
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
+pdf_path = path("2019_dbc.pdf")
+with open(file, "rb") as f:
+    base64_pdf = base64.b64encode(pdf_path.read_bytes()).decode("utf-8")
     # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-
+pdf_display = f"""
+    <iframe src="data:application/pdf;base64,{base64_pdf}" width="800px" height="2100px" type="application/pdf"></iframe>
+"""
+                  
     # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
+st.markdown(pdf_display, unsafe_allow_html=True)
 
-displayPDF('2019_dbc.pdf')
+# def displayPDF(file):
+#     # Opening file from file path
+#     pdf_path = path("2019_dbc.pdf")
+#     with open(file, "rb") as f:
+#         base64_pdf = base64.b64encode(pdf_path.read_bytes()).decode("utf-8")
+#     # Embedding PDF in HTML
+#     pdf_display = f"""
+#         <iframe src="data:application/pdf;base64,{base64_pdf}" width="800px" height="2100px" type="application/pdf"></iframe>
+#     """
+                  
+#     # Displaying File
+#     st.markdown(pdf_display, unsafe_allow_html=True)
+
+# displayPDF('2019_dbc.pdf')
 # st.markdown(hide_st_style, unsafe_allow_html=True)
