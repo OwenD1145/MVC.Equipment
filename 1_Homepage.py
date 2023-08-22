0245e0616895819a820bb2a4a3b672bf43b3d285
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-22 11:10:48 -0600

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -21,9 +21,9 @@ hide_st_style="""
             """
 
 
-def displayPDF():
+def displayPDF(file):
     # Opening file from file path
-    with open('MVC.Equipment/pages/2021 - Xcel-Energy-Standard-For-Electric-Installation-and-Use.pdf', "rb") as f:
+    with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
@@ -32,5 +32,5 @@ def displayPDF():
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-displayPDF()
+displayPDF('MVC.Equipment/pages/2021 - Xcel-Energy-Standard-For-Electric-Installation-and-Use.pdf')
 st.markdown(hide_st_style, unsafe_allow_html=True)
