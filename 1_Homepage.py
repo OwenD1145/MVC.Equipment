ee48eac951958873d31721591bdb8ce079d4dc0c
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-22 13:59:01 -0600

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -28,13 +28,16 @@ def displayPDF(file):
 
     # Embedding PDF in HTML
     global pdf_display
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     
-    # pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = (
+        f'<embed src="data:application/pdf;base64,{base64_pdf}" '
+        'width="800" height="1000" type="application/pdf"></embed>'
+    )
     
     # Displaying File
-    # st.markdown(pdf_display, unsafe_allow_html=True)
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 displayPDF("2019_dbc.pdf")
 # st.markdown(hide_st_style, unsafe_allow_html=True)
-st.markdown(pdf_display, unsafe_allow_html=True)
+# st.markdown(pdf_display, unsafe_allow_html=True)
