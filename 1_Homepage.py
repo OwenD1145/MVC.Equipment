9cc9086c0525958bc704bf579580fab8451479dd
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-22 14:28:31 -0600

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,43 +1,35 @@
 import streamlit as st
 import base64
 
-# st.set_page_config(
-#   page_title="MVC Tools - Homepage",
-#   page_icon=":bar_chart:",
-#   layout="wide"                 
-# )
+st.set_page_config(
+  page_title="MVC Tools - Homepage",
+  page_icon=":bar_chart:",
+  layout="wide"                 
+)
 
-# # MAINPAGE
+# MAINPAGE
 
-# st.header("MVC Tools of the Trade")
-# st.markdown("##")
+st.header("MVC Tools of the Trade")
+st.markdown("##")
 
 
-# hide_st_style="""
-#             <style>
-#             #MainMenu {visibility:hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
+hide_st_style="""
+            <style>
+            #MainMenu {visibility:hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
-    global pdf_display
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    
-    # pdf_display = (
-    #     f'<embed src="data:application/pdf;base64,{base64_pdf}" '
-    #     'width="800" height="1000" type="application/pdf"></embed>'
-    # )
-    
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-displayPDF("2019_dbc.pdf")
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+# def displayPDF(file):
+#     # Opening file from file path
+#     with open(file, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     # Embedding PDF in HTML
+#     global pdf_display
+#     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+#     # Displaying File
+#     st.markdown(pdf_display, unsafe_allow_html=True)
+# displayPDF("2019_dbc.pdf")
+st.markdown(hide_st_style, unsafe_allow_html=True)
 # st.markdown(pdf_display, unsafe_allow_html=True)
