8d267b9a2ffb6c0b98a1b774d1afbcea33629089
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2023-08-22 10:55:12 -0600

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -23,7 +23,7 @@ hide_st_style="""
 
 def displayPDF():
     # Opening file from file path
-    with open(2021 - Xcel-Energy-Standard-For-Electric-Installation-and-Use.pdf, "rb") as f:
+    with open((2021 - Xcel-Energy-Standard-For-Electric-Installation-and-Use.pdf), "rb") as f:
         base64_pdf = base64.b64encode(f.read(2021 - Xcel-Energy-Standard-For-Electric-Installation-and-Use.pdf)).decode('utf-8')
 
     # Embedding PDF in HTML
