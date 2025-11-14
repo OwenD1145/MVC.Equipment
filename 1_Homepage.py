e3630a1cc65f431488fa37606e366ffb47406550
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2025-11-14 09:36:10 -0700

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -54,7 +54,7 @@ with tabs[0]:
         with col1:
             voltage_3ph = st.number_input("Line Voltage (V)", value=480.0, key="voltage_3ph")
         with col2:
-            current_3ph = st.number_input("Line Current (A)", value=50.0, key="current_3ph")
+            current_3ph = st.number_input("Line Current (FLA)", value=50.0, key="current_3ph")
         
         if st.button("Calculate Three Phase VA"):
             va_3ph = (math.sqrt(3) * voltage_3ph) * (current_3ph * 1.25)
